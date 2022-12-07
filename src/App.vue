6bd1005729084b53965f68649899f76646964e32
RominaMalimban <romina.lyn@hotmail.it>
2022-12-07 19:24:10 +0100

    esercizio completato

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
-<script setup>
+<script>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import AppTitle from './components/AppTitle.vue'
+
+export default {
+  name: "App",
+  components: {
+    AppTitle
+  }
+}
 </script>
 
 <template>
@@ -13,7 +20,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppTitle msg="Vite + Vue" />
 </template>
 
 <style scoped>
@@ -22,9 +29,11 @@ import HelloWorld from './components/HelloWorld.vue'
   padding: 1.5em;
   will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: "AppTitle",
+    data() {
+        return {
+            msg: "La mia prima app con Vite"
+        }
+    }
+}
+
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+
+
+</template>
+
+<style scoped>
+h1 {
+    color: #9d5cfe;
+    font-style: italic;
+}
+</style>
